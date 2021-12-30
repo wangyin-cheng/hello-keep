f7082381abcd7b9080fdb5d1c76368c2ca29223e
wangTu <2413823269@qq.com>
2021-12-30 09:59:10 +0800

    test

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {Message, Button} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Message)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <el-button type="primary" @click="testFunc">主要按钮</el-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -13,6 +14,12 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    testFunc() {
+       // this.$message.success("XXX成功")
+       console.log(122, this.$message);
+    }
   }
 }
 </script>
